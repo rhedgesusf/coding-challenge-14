488c88c02ecb043dabe79c83823f107557da9f8d
Riley Hedges <rhedges1@usf.edu>
2025-03-07 18:58:25 -0500

    Task 2 - Adding Support Tickets Dynamically

--- cc_14.js
+++ cc_14.js
@@ -18,18 +18,42 @@ function createTicket(name, description, priority) {
     const ticketContainer = document.getElementById("ticketContainer");
   
     if (ticketContainer) {
-      const ticket = document.createElement("div");
+      const card = document.createElement("div");
   
       card.setAttribute("class", "ticket-card");
       card.setAttribute("id", "ticketCard");
   
      setTicketDetails(card, name, description, priority);
   
-      card.addEventListener("dblclick", (event) => {
-        editTicketCard(card);
-      });
-  
       ticketContainer.appendChild(card);
     }
   }
+
+  function setTicketDetails(card, name, description, priority) {
+    card.innerHTML = `<div><h3>${name}</h3>
+      <p>${description}</p>
+      <label>${priority}</label>
+      </div>`;
   
+    // create a button
+    const button = document.createElement("button");
+    button.textContent = "Resolve";
+  
+    // add click listener
+    button.addEventListener("click", (event) => {
+      // remove from container
+      ticketContainer.removeChild(card);
+      event.stopPropagation();
+    });
+    card.appendChild(button);
+  }
+  
+createTicket('Joe Johnson', "Password Issue", 'High')
+createTicket('Bill Myers', "Cracked Screen", 'Medium')
+createTicket('Mike Mob', "Broken Cable", 'Low')
+createTicket('Ethan Pitta', "Stolen Laptop", 'High')
+
+
+
+
+
